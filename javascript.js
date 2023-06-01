b0b4f291ba5a280a454085f872b1ce638c9f322e
paulywrath <127055825+paulywrath@users.noreply.github.com>
2023-05-31 23:30:41 -0400

    Throw rock, paper, or scissors by computer

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-function getComputerChoice
+function getComputerChoice() {
+    fist = Math.floor(Math.random() * 3);
+    if (fist === 0) {
+        return 'Rock';
+    } else if (fist === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+getComputerChoice()
