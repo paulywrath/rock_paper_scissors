f242b5544fedd32a59d9db5fe3e2108d662110bf
paulywrath <127055825+paulywrath@users.noreply.github.com>
2023-06-05 20:17:02 -0400

    See whose throw wins or if they're equal.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,32 @@
+//The computer opponent throws “rock,” “paper,” or “scissors” randomly.//
 function getComputerChoice() {
+    
     fist = Math.floor(Math.random() * 3);
     if (fist === 0) {
-        return 'Rock';
+        return 'ROCK';
     } else if (fist === 1) {
-        return 'Paper';
+        return 'PAPER';
     } else {
-        return 'Scissors';
+        return 'SCISSORS';
     }
+    
 }
 
 getComputerChoice()
+
+//See whose throw wins or if they’re equal.//
+function playRound(playerSelection, computerSelection) {
+    
+    playerSelection.toUpperCase();
+
+    if (playerSelection === computerSelection) {
+        return `Tie! You both threw ${playerSelection}.`;
+    } else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+    (playerSelection === 'PAPER' && computerSelection === 'ROCK')) {
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+
+  }
