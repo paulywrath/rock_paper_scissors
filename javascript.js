eaca2da4474161b51f6ba58f43380e67f722c4a1
paulywrath <127055825+paulywrath@users.noreply.github.com>
2023-06-05 23:00:24 -0400

    Attempt to write function to play a 5 round game
    I think the problem is that I don't know how to make the following
    function re-run to get a new result to play multiple rounds:
    
    getComputerChoice(), which makes the computer opponent throw “rock,”
    “paper,” or “scissors” randomly.

--- javascript.js
+++ javascript.js
@@ -9,10 +9,8 @@ function getComputerChoice() {
     } else {
         return 'SCISSORS';
     }
-    
-}
 
-getComputerChoice()
+}
 
 //See whose throw wins or if they’re equal.//
 function playRound(playerSelection, computerSelection) {
@@ -30,3 +28,36 @@ function playRound(playerSelection, computerSelection) {
     }
 
   }
+
+//Play a five round game.//
+function game() {
+
+    getComputerChoice(); //Computer throws its hand.//
+    playRound(playerSelection, computerSelection); //See who wins.//
+    
+    //Add to winner's score.//
+    if (roundResult.includes('win')) {
+        return playerScore += 1;
+    } else if (roundResult.includes('lose')) {
+        return computerScore += 1;
+    }
+
+}
+
+const playerSelection = 'ROCK'; //Player throws rock.//
+
+//Create variable to go into winner check that accepts computer's throw.//
+let computerSelection = getComputerChoice();
+
+/*Create variable that receives the result of the round 
+and checks the string to pass who won into an operation.*/
+let roundResult = playRound(playerSelection, computerSelection);
+
+let playerScore = 0; //Create variable to hold player's score.//
+let computerScore = 0; //Create variable to hold computer's score.//
+
+game(); //Call the function that plays a multi-round game.//
+console.log(computerSelection); //Show what the computer threw.//
+
+//Show the round's result to the user.//
+console.log(playRound(playerSelection, computerSelection)); 
