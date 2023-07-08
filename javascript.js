297bd143c65fd8fa7fdbce29854c758154d072c4
paulywrath <127055825+paulywrath@users.noreply.github.com>
2023-07-08 17:42:01 -0400

    Clean up code according to my observations

--- javascript.js
+++ javascript.js
@@ -8,50 +8,28 @@ let roundResult; //Create variable that receives the round result.
 let playerScore = 0; //Create variable to hold player's score.
 let computerScore = 0; //Create variable to hold computer's score.
 
-const computerThrow = document.getElementById("computer-throw"); //Attach node to "computer-throw" div.
+const intro = document.getElementById("intro"); //Attach node to "intro" div.
+
+const game = document.getElementById("game"); //Attach node to "game" div.
+
 const playerThrow = document.getElementById("player-throw"); //Attach node to "player-throw" div.
+const computerThrow = document.getElementById("computer-throw"); //Attach node to "computer-throw" div.
 
 const showResult = document.getElementById("show-result"); //Attach node to "show-result" div. 
 
 const score = document.getElementById("score"); //Attach node to "score" div.
 
-const intro = document.getElementById("intro"); //Attach node to "intro" div.
-
-const game = document.getElementById("game"); //Attach node to "game" div.
-
-// buttons is a node list. It looks and acts much like an array.
+// Attach node to buttons element. It is a node list of the buttons, which acts much like an array.
 const buttons = document.querySelectorAll('button');
 
-// we use the .forEach method to iterate through each button
+// When you click on a button, this event listener goes into action.
 buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
   button.addEventListener('click', () => {
-    playerSelection = button.id;
-    intro.textContent = '';
-    playRound();
+    playerSelection = button.id; //Assign button pressed to player selection variable.
+    intro.textContent = ''; //Remove intro text when player starts the game.
+    playRound(); //Play a round.
   });
 });
-
-function gameover() {
-    if (playerScore === 5) {
-        intro.textContent = '\r\nYou win the match! Unbelievable! \
-You are the new Rock, Paper, Scissors world champion!';
-    } else if (playerScore < 4 && computerScore === 5) {
-        intro.textContent = '\r\nYou lose the match. You\'re brave but you never had a chance.';
-    } else if (playerScore === 4 && computerScore === 5) {
-        intro.textContent = '\r\nYou lose the match. Will you brag that you came close \
-to anyone who will listen, or will you try again?';
-    }
-    
-    if (playerScore === 5 || computerScore === 5) {
-        intro.setAttribute('style', 'white-space: pre; color: white;');
-        game.setAttribute('style', 'white-space: pre;');
-        game.textContent = '\r\nRefresh the page to play again.';
-    }
-}
-
-//Call the function that plays a multi-round game. game();
 
 //The computer opponent throws “rock,” “paper,” or “scissors” randomly.
 function getComputerChoice() {
@@ -82,6 +60,26 @@ function seeWhoWins(playerSelection, computerSelection) {
 
   }
 
+// Display "game over" message when someone reaches 5 points.
+function gameover() {
+
+    if (playerScore === 5) {
+        intro.textContent = '\r\nYou win the match! Unbelievable! \
+You are the new Rock, Paper, Scissors world champion!';
+    } else if (playerScore < 4 && computerScore === 5) {
+        intro.textContent = '\r\nYou lose the match. You\'re brave but you never had a chance.';
+    } else if (playerScore === 4 && computerScore === 5) {
+        intro.textContent = '\r\nYou lose the match. Will you brag that you came close \
+to anyone who will listen, or will you try again?';
+    }
+    
+    if (playerScore === 5 || computerScore === 5) {
+        intro.setAttribute('style', 'white-space: pre; color: white;');
+        game.setAttribute('style', 'white-space: pre; color: white;');
+        game.textContent = '\r\nRefresh the page to play again.';
+    }
+}
+ 
 //Play a round.
 function playRound() {    
         getComputerChoice(); //Computer throws its hand.
@@ -89,13 +87,11 @@ function playRound() {
         computerThrow.textContent = computerSelection; //Show what the computer threw.
         playerThrow.textContent = playerSelection; //Show what the player threw.
         seeWhoWins(playerSelection, computerSelection); //See who wins.
-        whoWon = seeWhoWins(playerSelection, computerSelection);
+            //Assign round result to variable for following score counter.
+        roundResult = seeWhoWins(playerSelection, computerSelection); 
 
         //Show the round's result to the user.
-        showResult.textContent = whoWon; 
-
-        //Assign round result to variable for following score counter.
-        roundResult = whoWon; 
+        showResult.textContent = roundResult; 
             
         //Style winner's throw
         playerThrow.style.cssText = 'font-weight: normal; text-decoration: none;';
@@ -119,17 +115,6 @@ function playRound() {
         score.textContent += `You: ${playerScore}\r\n` 
         score.textContent += `Omniscient Fist®: ${computerScore}`;
 
+        // Check if anyone has 5 points.
         gameover();
 }
-
-  /*Play a five round game.
-function game() {
-
-    if (playerScore > computerScore) {
-        console.log('Congratulations, you won the game!');
-    } else if (playerScore < computerScore) {
-        console.log('You lost the game. Better luck next time.');
-    } else {
-        console.log('Kiss your sister, it\'s a tie.');
-    }
-}*/
